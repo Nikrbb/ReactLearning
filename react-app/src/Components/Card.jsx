606da46bc00fa57349c1887f04b1467f1b8b8d9e
Nikrbb <81448903+Nikrbb@users.noreply.github.com>
2022-05-31 15:32:39 +0300

    refactor

--- react-app/src/Components/Card.jsx
+++ react-app/src/Components/Card.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 
-function TitleText (props) {
-    return !props ? null :
-                props.title ?
-                    <h4 className="card-title">{props.title}</h4> :
-                    <p className="card-text">{props.text}</p>
-}
-TitleText()
 class Card extends React.Component {
-
+    TitleText (props) {
+        return !props ? null :
+            props.title ?
+                <h4 className="card-title">{props.title}</h4> :
+                <p className="card-text">{props.text}</p>
+    }
     render() {
         const { title, text } = this.props;
         return  <>
                     <div className="card">
                         <div className="card-body">
-                            <TitleText title={title}/>
-                            <TitleText text={text} />
+                            <this.TitleText title={title}/>
+                            <this.TitleText text={text} />
                         </div>
                     </div>
                 </>
