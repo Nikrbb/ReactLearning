b9d3d2e2b77bcbb2383ce934e5cf6e25dff90f75
Nikrbb <81448903+Nikrbb@users.noreply.github.com>
2022-11-11 21:24:12 +0200

    fixed key in definitions

--- react-app/src/Components/Definitions.jsx
+++ react-app/src/Components/Definitions.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 class Definitions extends React.Component {
-    renderList() {
-        const { data } = this.props
-        return data.map(item => {
-            return <>
-                <dt key={item.id}>{item.dt}</dt>
-                <dd>{item.dd}</dd>
-            </>
-        })
-    }
     render() {
-        return <dl>{this.renderList()}</dl>
+        const { data } = this.props;
+        return (
+            <dl>
+                {data.map(item => <React.Fragment key={item.id}>
+                    <dt>{ item.dt }</dt>
+                    <dd>{ item.dd }</dd>
+                </React.Fragment>)}
+            </dl>
+        )
     }
 }
 
