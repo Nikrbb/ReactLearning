19875a7820fc9fc23c44fcee90a0d4df2a5da26e
Nikrbb <81448903+Nikrbb@users.noreply.github.com>
2022-11-11 21:06:29 +0200

    simplified check

--- react-app/src/Components/Card.jsx
+++ react-app/src/Components/Card.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 
 class Card extends React.Component {
-    TitleText (props) {
-        return !props ? null :
-            props.title ?
-                <h4 className="card-title">{props.title}</h4> :
-                <p className="card-text">{props.text}</p>
-    }
     render() {
         const { title, text } = this.props;
         return  <>
                     <div className="card">
                         <div className="card-body">
-                            <this.TitleText title={title}/>
-                            <this.TitleText text={text} />
+                            {<h4 className="card-title">{ title }</h4> ?? null}
+                            {<p className="card-text">{ text }</p> ?? null}
                         </div>
                     </div>
                 </>
